1f297e0c7a70637862762ef192303f784fc18839
Thibault Duval <gerardufoin2@hotmail.com>
2018-01-09 17:24:44 +0100

    Parser is parsing

--- src/QuizParser.js
+++ src/QuizParser.js
@@ -4,10 +4,7 @@ var fs = require('fs');
 
 class QuizParser {
 	constructor() {
-		this.testQuestions =  [
-			{ 'question': "Quel animal?", 'answer': new RegExp("^(un )?chien$|^(un )?chat$", "gi"), 'hint': "C...", 'finalAnswer': 'Un chat' },
-			{ 'question': "Quelle nourriture?", 'answer': new RegExp("^(le (petit )?)?poulet$|^cassoulet$", "gi"), 'hint': "P...", 'finalAnswer': 'Du poulet' }
-		];
+		this.testQuestions = [];
 	}
 
 	parse(callback) {
@@ -17,58 +14,187 @@ class QuizParser {
 				callback("Unable to read quizzes directory.", []);
 				return;
 			}
-			for (let i = 0; i < items.length; ++i) {
-				if (items != 'README') {
-					callback(null, this.testQuestions);
-					return ;
+			for (let i = 0; i < items.length;) {
+				let sp = items[i].split('.');
+				if (sp.length > 1 && sp[sp.length - 1] === 'txt') {
+					++i;
+				} else {
+					items.splice(i, 1);
 				}
 			}
-			callback("No quiz available.", []);
+			if (!items.length) {
+				callback("No quiz available.", []);
+				return;				
+			}
+			let parsed = 0;
+			let error = '';
+			for (let i = 0; i < items.length; ++i) {
+				fs.readFile('./quizzes/' + items[i], "latin1", (err, data) => {
+					if (err) {
+						console.log('Readfile error: ' + items[i] + ': ' + err);
+						error += 'Unable to read ' + items[i] + '.';
+					} else if (!error) {
+						this.parseFileContent(data);
+					}
+					parsed++;
+					if (parsed >= items.length) {
+						if (error) {
+							callback(error, []);
+						} else {
+							console.log(this.testQuestions);
+							callback(null, this.testQuestions);							
+						}
+					}
+				});
+			}
 		});
+	}
+
+	parseFileContent(content) {
+		// Remove comments
+		content = content.replace(/(\/{3,}.*(?:\r?\n|\r)?)/g, "");
+		let questions = content.match(/((?:.+(?:\r?\n|\r)?)+)/g);
+		for (let i = 0; i < questions.length; ++i) {
+			this.parseQuestion(questions[i]);
+		}
+	}
+
+	parseQuestion(question) {
+		let sp = question.split(/\r?\n|\r/g);
+		// Remove last element if it does not contain characters
+		if (!/\S/.test(sp[sp.length - 1])) {
+			sp.pop();
+		}
+		if (sp.length < 2) {
+			console.log("Warning: Missing question or answer for the block:\n" + question + "\nQuestion ignored.");
+			return;
+		}
+		sp = this.cleanUp(sp);
+		let holes = (sp[0].match(/___/g) || []).length;
+		if (this.checkAnswersFormat(question, sp, holes)) {
+			if (holes) {
+				this.parseMultipleFormat(question, sp);
+			} else {
+				this.parseSingleFormat(question, sp);
+			}
+		}
+	}
+
+	cleanEmptySlots(array) {
+		for (let i = 0; i < array.length;) {
+			if (/\S/.test(array[i])) {
+				++i;
+			} else {
+				array.splice(i, 1);
+			}
+		}
+		return array;
+	}
+
+	cleanUp(sp) {
+		sp[0] = sp[0].trim().replace(/  +/g, ' ');
+		for (let i = 1; i < sp.length; ++i) {
+			sp[i] = this.cleanEmptySlots(sp[i].trim().replace(/  +/g, ' ').split(/ ?-> ?/g));
+			if (sp[i].length > 1) {
+				sp[i][0] = this.cleanEmptySlots(sp[i][0].split(/ ?, ?/g));
+			}
+		}
+		return sp;
+	}
+
+	checkAnswersFormat(question, sp, holes) {
+		return true;
+	}
+
+	escapeRegExp(text) {
+		return text.replace(/[-[\]{}*+?.,\\^$|#\s]/g, '\\$&');
+	}
+
+	swapIfFirstOption(answer) {
+		if (answer[0] == '(') {
+			let par = 0;
+			for (let i = 0; i < answer.length; ++i) {
+				if (answer == '(') {
+					par++;
+				}
+				if (answer == ')') {
+					par--;
+					if (par == 0) {
+						if (i < answer.length - 1 && answer[i + 1] == ' ') {
+							answer[i + 1] = ')';
+							while (answer[i--] == ')');
+							answer[i + 1] = ' ';
+							return answer;
+						}
+					}
+				}
+			}
+		}
+		return answer;
+	}
+
+	createRegExpAnswer(answers) {
+		let sp = answers.split(/\s*,\s*/g);
+		for (let i = 0; i < sp.length; ++i) {
+			sp[i] = this.escapeRegExp(this.swapIfFirstOption(sp[i]).replace(/\s(\(+)/g, '$1 '));
+			sp[i] = sp[i].replace(/\)/g, ')?');
+		}
+		return new RegExp('^' + sp.join('|') + '$', 'gi');
+	}
+
+	createFinalAnswer(answer) {
+		return (answer.replace(/[()]/g, ''));
+	}
+
+	createHint(answer) {
+		let start = -1;
+		let par = 0;
+		for (let i = 0; i < answer.length; ++i) {
+			if (answer[i] == '(') {
+				par++;
+				if (start < 0) {
+					start = i;
+				}
+			}
+			if (answer[i] == ')') {
+				par--;
+				if (par == 0) {
+					if (start > -1) {
+						answer = answer.replace(answer.substring(start, i + 1), '');
+						i = start - 1;
+					}
+					start = -1;
+				}
+			}
+		}
+		return answer.trim()[0] + '...';
+	}
+
+	parseSingleFormat(question, sp) {
+		let fAnswer = sp[1][0].split(',')[0];
+		this.testQuestions.push({
+			'question': sp[0],
+			'answer': this.createRegExpAnswer(sp[1][0]),
+			'hint': this.createHint(fAnswer),
+			'finalAnswer': this.createFinalAnswer(fAnswer)
+		});
+	}
+
+	parseMultipleFormat(question, sp) {
+		for (let i = 1; i < sp.length; ++i) {
+			let q = sp[0];
+			for (let h = 0; h < sp[i][0].length; ++h) {
+				q = q.replace(/___/, sp[i][0][h]);
+			}
+			let fAnswer = sp[i][1].split(',')[0];
+			this.testQuestions.push({
+				'question': q,
+				'answer': this.createRegExpAnswer(sp[i][1]),
+				'hint': this.createHint(fAnswer),
+				'finalAnswer': this.createFinalAnswer(fAnswer)
+			});
+		}
 	}
 }
 
 module.exports = QuizParser;
-
-/// <summary>
-/// Format:
-/// 
-/// - Questions are separated by at least two blank lines.
-/// - The first lines of a question contains the question paragraphs, where ___ (three underscores) denote holes.
-/// - Next lines contain hole values (for all paragraphs) separated by commas, 
-///   followed by -> then the possible answers, also separated by commas.
-/// - If there are no holes, the answers must be preceded by -> anyway.
-/// - Answers may contain parenthesized portions, which are optional.
-/// 
-/// Who ran against Jimmy Carter in 1976?
-/// -> (Gerald (R.)) Ford
-/// 
-/// Who was elected President of ___ in ___?
-/// the United States, 2008 -> (Barack) Obama
-/// France, 2012 -> (François) Hollande
-/// 
-/// In which year was ___ elected President of the United States?
-/// Franklin D. Roosevelt -> 1932, 1936, 1940, 1944
-/// Harry S Truman -> 1948
-/// 
-/// This country was ruled by George W. Bush from 2000 to 2008.
-/// It led the First World during the Cold War.
-/// -> (The) United States, US(A), U.S.(A.)
-/// </summary>
-
-///((?:.+\n{1})+[^\n]+)
-
-
-/*function escapeRegExp(text) {
-  return text.replace(/[-[\]{}*+?.,\\^$|#\s]/g, '\\$&');
-}
-
-var test = escapeRegExp('(Gerald (R.)) Ford'.replace(/(\)+) /g, ' $1')).replace(/\)/g, ')?');
-test = '^' + test + '$';
-
-var reg = new RegExp(test, 'gi');
-
-console.log(reg.test('gerald r. ford'));
-console.log(reg.test('gerald r.ford'));
-console.log(/^(Gerald (R\. )?)?Ford$/gi.test('gerald r. ford'));
-console.log(/^(Gerald (R\. )?)?Ford$/gi.test('gerald r.ford'));*/
